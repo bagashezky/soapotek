c749f1dcbc6b01cd4e941f1f2aafaa104cf403dc
bagashezky <bagashpratama95@gmail.com>
2023-07-08 05:48:39 +0700

    db

--- Backup DB/ci-apotek.sql
+++ Backup DB/ci-apotek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 21 Bulan Mei 2023 pada 10.41
+-- Waktu pembuatan: 08 Jul 2023 pada 00.27
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.10
 
@@ -20,6 +20,48 @@ SET time_zone = "+00:00";
 --
 -- Database: `ci-apotek`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `akun`
+--
+
+CREATE TABLE `akun` (
+  `no_reff` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `nama_reff` varchar(40) NOT NULL,
+  `keterangan` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `akun`
+--
+
+INSERT INTO `akun` (`no_reff`, `id_user`, `nama_reff`, `keterangan`) VALUES
+(111, 1, 'Kas', 'Kas Peruangan'),
+(112, 1, 'Persediaan Barang', 'Persediaan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `coa`
+--
+
+CREATE TABLE `coa` (
+  `kode_coa` int(11) NOT NULL,
+  `nama_coa` varchar(50) NOT NULL,
+  `header_akun` varchar(7) NOT NULL,
+  `posisi_db_cr` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `coa`
+--
+
+INSERT INTO `coa` (`kode_coa`, `nama_coa`, `header_akun`, `posisi_db_cr`) VALUES
+(111, 'Kas', '1', 'Debitt'),
+(114, 'Persediaan', '2', 'Debit');
 
 -- --------------------------------------------------------
 
@@ -102,6 +144,7 @@ INSERT INTO `month` (`month_num`, `month_name`) VALUES
 
 CREATE TABLE `obats` (
   `id_obat` int(4) NOT NULL,
+  `kode_obat` varchar(7) NOT NULL,
   `nama_obat` varchar(30) NOT NULL,
   `jmlh_stok` int(11) NOT NULL,
   `unit` varchar(20) NOT NULL,
@@ -118,9 +161,14 @@ CREATE TABLE `obats` (
 -- Dumping data untuk tabel `obats`
 --
 
-INSERT INTO `obats` (`id_obat`, `nama_obat`, `jmlh_stok`, `unit`, `nama_kategori`, `tgl_beli`, `tgl_kadaluarsa`, `des_obat`, `harga_obat`, `harga_jual`, `nama_supplier`) VALUES
-(1052, 'OSKADON', 32, 'Kapsul', 'Obat Bebas', '2023-05-21', '2023-09-29', '', 1000, 2000, 'bagas'),
-(1053, 'Mixagrip', 18, 'Uap', 'Obat Keras', '2023-05-21', '2023-05-31', '', 12222, 122222, 'bagas');
+INSERT INTO `obats` (`id_obat`, `kode_obat`, `nama_obat`, `jmlh_stok`, `unit`, `nama_kategori`, `tgl_beli`, `tgl_kadaluarsa`, `des_obat`, `harga_obat`, `harga_jual`, `nama_supplier`) VALUES
+(1052, '', 'OSKADON', 143, 'Kapsul', 'Obat Bebas', '2023-05-21', '2023-09-29', '', 1000, 2000, 'bagas'),
+(1053, '', 'Mixagrip', 19, 'Uap', 'Obat Keras', '2023-05-21', '2023-05-31', '', 12222, 122222, 'bagas'),
+(1059, 'BL72593', 'Bodrexin', 122, 'Tablet', 'Obat Bebas', '2023-06-12', '2023-07-08', '', 1000, 2000, 'bagas'),
+(1061, 'BL79729', 'Mixagripss', 53, 'Sirup', 'Obat Keras', '2023-07-18', '2023-07-26', '', 122, 12121, 'bagas'),
+(1063, 'BL52265', 'Mixagripss', 162, 'Cair', 'Obat Bebas', '2023-07-11', '2023-07-19', '', 1313, 11414, 'Komia Farmu'),
+(1064, 'BL93566', 'OSKADON', 122, 'Kapsul', 'Obat Keras', '2023-07-05', '2023-07-20', '', 12122, 1212, 'Komia Farmu'),
+(1065, 'BL43842', 'paramex', 561, 'Cair', 'Obat Keras', '2023-07-04', '2023-08-05', '', 2000, 5000, 'Komia Farmu');
 
 -- --------------------------------------------------------
 
@@ -145,10 +193,8 @@ CREATE TABLE `pembelian` (
 --
 
 INSERT INTO `pembelian` (`id_pembelian`, `ref`, `nama_obat`, `harga_obat`, `banyak`, `subtotal`, `nama_supplier`, `tgl_beli`, `grandtotal`) VALUES
-(34, 'BL34123231', 'OSKADON', 1000, 1, 1000, 'bagas', '2023-05-21', 1000),
-(35, 'BL73593657', 'Mixagrip', 12222, 2, 24444, 'bagas', '2023-06-08', 24444),
-(36, 'BL99312168', 'Mixagrip', 12222, 2, 24444, 'bagas', '2023-07-12', 24444),
-(37, 'BL18225879', 'Mixagrip', 12222, 1, 12222, 'bagas', '2023-08-17', 12222);
+(10, 'BL41967386', 'Mixagripss', 1313, 1, 1313, 'Komia Farmu', '2023-07-06', 1313),
+(11, 'BL92593576', 'paramex', 2000, 1, 2000, 'Komia Farmu', '2023-07-06', 2000);
 
 -- --------------------------------------------------------
 
@@ -188,7 +234,7 @@ CREATE TABLE `supplier` (
 --
 
 INSERT INTO `supplier` (`id_supplier`, `nama_supplier`, `alamat`, `no_telp`) VALUES
-(110, 'bagas', 'bandung', '2144124'),
+(110, 'bagas', 'bandung', '081296283121'),
 (111, 'Komia Farmu', 'Bandung', '088888');
 
 -- --------------------------------------------------------
@@ -209,30 +255,86 @@ CREATE TABLE `table_invoice` (
   `grandtotal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data untuk tabel `table_invoice`
+-- Struktur dari tabel `table_user`
 --
 
-INSERT INTO `table_invoice` (`id_tagihan`, `ref`, `nama_obat`, `harga_jual`, `banyak`, `subtotal`, `nama_pembeli`, `tgl_beli`, `grandtotal`) VALUES
-(80, 'JMBSErmmo0', 'Adona', 15000, 1, 15000, 'Melinda', '2018-08-09', 29000),
-(81, 'JMBSErmmo0', 'Asam Mefenamat', 4000, 1, 4000, 'Melinda', '2018-08-09', 29000),
-(82, 'JMBSErmmo0', 'Salbutamol', 10000, 1, 10000, 'Melinda', '2018-08-09', 29000),
-(87, 'xl23Q6whsP', 'Salbutamol', 10000, 3, 30000, 'Melinda', '2018-01-10', 30000),
-(90, '9ab7RcZHma', 'Adrome', 15000, 6, 90000, 'Doni', '2018-02-14', 90000),
-(91, 'fAMEjr9rA0', 'Ambroxol', 22000, 5, 110000, 'Amirullah', '2018-03-14', 110000),
-(92, 'so5518T1vI', 'Salbutamol', 10000, 6, 60000, 'Doni', '2018-04-18', 60000),
-(97, 'VS27jjD5Ze', 'Ambroxol', 22000, 1, 22000, 'Pitaloka', '2018-07-18', 32000),
-(98, 'VS27jjD5Ze', 'Stimuno', 10000, 1, 10000, 'Pitaloka', '2018-07-18', 32000),
-(99, 'nyptFm9i43', 'Stimuno', 10000, 3, 30000, 'Andi', '2018-06-13', 30000),
-(101, 'e7QgBweE8X', 'Adrome', 15000, 1, 15000, 'Andi', '2018-09-15', 15000),
-(105, 'zPpAfLsmd7', 'Adrome', 15000, 1, 15000, 'Amila', '2018-05-16', 25000),
-(106, 'zPpAfLsmd7', 'Amoxilin', 10000, 1, 10000, 'Amila', '2018-05-16', 25000),
-(119, 'FfndSkUmh6', 'Asam Mefenamat', 4000, 2, 8000, 'Alina', '2018-09-24', 12000),
-(120, 'FfndSkUmh6', 'Vitamin E', 4000, 1, 4000, 'Alina', '2018-09-24', 12000);
+CREATE TABLE `table_user` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_password` varchar(50) NOT NULL,
+  `user_akses` int(11) NOT NULL,
+  `user_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `table_user`
+--
+
+INSERT INTO `table_user` (`user_id`, `user_name`, `user_email`, `user_password`, `user_akses`, `user_status`) VALUES
+(1, 'Tuanku Imam Bonjol', 'admin@gmail.com', 'admin123', 1, 1),
+(2, 'Muhammad Hatta', 'apoteker@gmail.com', 'apoteker', 2, 1),
+(3, 'Bagindo Aziz Chan', 'pegawai@gmail.com', 'pegawai', 3, 1),
+(4, 'pemilik', 'pemilik@gmail.com', 'pemilik', 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_jurnal_umum`
+--
+
+CREATE TABLE `tbl_jurnal_umum` (
+  `no` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama_perkiraan` varchar(45) NOT NULL,
+  `debet` int(11) NOT NULL,
+  `kredit` int(11) NOT NULL,
+  `keterangan` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_jurnal_umum`
+--
+
+INSERT INTO `tbl_jurnal_umum` (`no`, `tanggal`, `nama_perkiraan`, `debet`, `kredit`, `keterangan`) VALUES
+(1, '2023-07-06', 'Pembelian', 1313, 0, 'Tunai'),
+(2, '2023-07-06', 'Kas', 0, 1313, 'Pembelian');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `no_reff` int(11) NOT NULL,
+  `tgl_input` datetime NOT NULL,
+  `tgl_transaksi` date NOT NULL,
+  `jenis_saldo` enum('debit','kredit','','') NOT NULL,
+  `saldo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`no_reff`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `coa`
+--
+ALTER TABLE `coa`
+  ADD PRIMARY KEY (`kode_coa`);
 
 --
 -- Indeks untuk tabel `jenis_obat`
@@ -260,7 +362,6 @@ ALTER TABLE `month`
 --
 ALTER TABLE `obats`
   ADD PRIMARY KEY (`id_obat`),
-  ADD UNIQUE KEY `nama_obat` (`nama_obat`),
   ADD KEY `med_unit` (`unit`),
   ADD KEY `med_cat` (`nama_kategori`),
   ADD KEY `med_sup` (`nama_supplier`);
@@ -292,8 +393,34 @@ ALTER TABLE `table_invoice`
   ADD PRIMARY KEY (`id_tagihan`);
 
 --
+-- Indeks untuk tabel `table_user`
+--
+ALTER TABLE `table_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indeks untuk tabel `tbl_jurnal_umum`
+--
+ALTER TABLE `tbl_jurnal_umum`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `no_reff` (`no_reff`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `coa`
+--
+ALTER TABLE `coa`
+  MODIFY `kode_coa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=115;
 
 --
 -- AUTO_INCREMENT untuk tabel `jenis_obat`
@@ -311,13 +438,13 @@ ALTER TABLE `kategori_obat`
 -- AUTO_INCREMENT untuk tabel `obats`
 --
 ALTER TABLE `obats`
-  MODIFY `id_obat` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1059;
+  MODIFY `id_obat` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1066;
 
 --
 -- AUTO_INCREMENT untuk tabel `pembelian`
 --
 ALTER TABLE `pembelian`
-  MODIFY `id_pembelian` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id_pembelian` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT untuk tabel `rak_penyimpanan`
@@ -335,7 +462,25 @@ ALTER TABLE `supplier`
 -- AUTO_INCREMENT untuk tabel `table_invoice`
 --
 ALTER TABLE `table_invoice`
-  MODIFY `id_tagihan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=121;
+  MODIFY `id_tagihan` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `table_user`
+--
+ALTER TABLE `table_user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_jurnal_umum`
+--
+ALTER TABLE `tbl_jurnal_umum`
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
